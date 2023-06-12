9178b1ea2c9479e7371e8c671498b27b76d0b3b7
Carlos Galiano <cachigaliano1997@gmail.com>
2023-06-12 08:16:37 -0300

    first commit

---
+++ app/components/header.jsx
@@ -0,0 +1,33 @@
+import { Link } from '@remix-run/react'
+import logo from '../../public/img/logo.svg'
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="contenedor barra">
+        <Link to="/">
+          <img className="logo" src={logo} alt="Imagen Logo" />
+        </Link>
+        <nav className="navegacion">
+          <Link 
+            to="/"
+          >Inicio</Link>
+
+          <Link 
+            to="/nosotros"
+          >Nosotros</Link>
+          
+          <Link 
+            to="/tienda"
+          >Tienda</Link>
+          
+          <Link 
+            to="/blog"
+          >Blog</Link>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
