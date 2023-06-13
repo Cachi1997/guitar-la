ac71e25754d27926d3d296252f79276960230965
Carlos Galiano <cachigaliano1997@gmail.com>
2023-06-13 18:32:08 -0300

    added css and guitar functionalities

---
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -0,0 +1,22 @@
+import { useLoaderData } from '@remix-run/react'
+import { getGuitarra } from '~/models/guitarras.server'
+
+export async function loader({params}){
+
+  const { guitarraUrl } = params
+  const guitarra = await getGuitarra(guitarraUrl)
+
+  return guitarra
+}
+
+function Guitarra() {
+
+  const guitarra = useLoaderData()
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes
+  
+  return (
+    <div>$guitarraUrl</div>
+  )
+}
+
+export default Guitarra
--- app/routes/nosotros.jsx
+++ app/routes/nosotros.jsx
@@ -1,6 +1,45 @@
+import imagen from '../../public/img/nosotros.jpg'
+import styles from '~/styles/nosotros.css'
+
+export function meta(){
+  return (
+    [
+      {title: 'GuitarLA - Sobre Nosotros'},
+      {description: 'Venta de Guitarras, blog de musica'}
+    ]
+  )
+}
+
+export function links(){
+  return [
+    {
+      rel: 'stylesheet',
+      href: styles
+    },
+    {
+      rel: 'preload',
+      href: imagen,
+      as: 'image'
+    }
+  ]
+}
+
 function Nosotros() {
   return (
-    <div>Nosotros</div>
+    <main className="contenedor nosotros">
+      <h2 className="heading">Nosotros</h2>
+      <div className="contenido">
+        <img src={imagen} alt='imagen sobre nosotros'/>
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet porta magna ac ornare. Donec sed dictum sapien. Aenean eu augue nisl. Morbi convallis a purus ac hendrerit. Nulla varius nunc ac mi tempus, viverra egestas sem suscipit. Mauris sit amet ante sit amet urna posuere malesuada. Mauris molestie rhoncus justo quis auctor.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet porta magna ac ornare. Donec sed dictum sapien. Aenean eu augue nisl. Morbi convallis a purus ac hendrerit. Nulla varius nunc ac mi tempus, viverra egestas sem suscipit. Mauris sit amet ante sit amet urna posuere malesuada. Mauris molestie rhoncus justo quis auctor.
+          </p>
+        </div>
+      </div>
+    </main>
   )
 }
 
